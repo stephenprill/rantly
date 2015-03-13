0854ccbdc0d4e25d56842a6faad1d69b333d1d49
Stephen Prill <stephen.prill@gmail.com>
2015-03-13 16:07:34 -0600

    user notice

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -8,7 +8,7 @@ class RegistrationsController < ApplicationController
       @user = User.new(user_params)
       if @user.save
         session[:user_id] = @user.id
-      redirect_to root_path, notice: "Thanks for signing up"
+        redirect_to root_path, notice: "Thanks for signing up"
     else
       render :new
     end
