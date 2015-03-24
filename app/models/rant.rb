1426e0927ccdb48f65991eae3082c51c5844b162
Stephen Prill <stephen.prill@gmail.com>
2015-03-24 09:54:54 -0600

    validated rant title

--- app/models/rant.rb
+++ app/models/rant.rb
@@ -3,6 +3,7 @@ class Rant < ActiveRecord::Base
   belongs_to :user
   validates_length_of :body, minimum: 144, :allow_blank => false #wont allow blank
   validates :body, presence: :true
+  validates :title, presence: :true
 
   def self.search(search)
     where("body ILIKE ? or title ILIKE ?", "%#{search}%", "%#{search}%")
